6cb4afe5ffe4bf047681ffffaeb0d62f23a88835
Ivanov Kirill <jcommander13@yandex.ru>
2021-05-03 18:45:31 +0500

    toolbar fixes

--- editor/components/toolbar/ui.py
+++ editor/components/toolbar/ui.py
@@ -15,21 +15,16 @@ class Toolbar(QtWidgets.QToolBar):
         self.setLayout(hbox)
         self.setMovable(False)
         self.setContextMenuPolicy(Qt.PreventContextMenu)
-        self.setFixedHeight(35)
 
         openFolder = QtWidgets.QToolButton()
         openFolder.setToolTip(self.tr('Shared.OpenFolder'))
         openFolder.setIcon(QtGui.QIcon(getFile('shared/icons/folder-open.svg')))
         openFolder.setToolButtonStyle(Qt.ToolButtonIconOnly)
-        openFolder.setFixedSize(QtCore.QSize(15, 15))
-        openFolder.setIconSize(QtCore.QSize(15, 15))
 
         runProcess = QtWidgets.QToolButton()
         runProcess.setToolTip(self.tr('Shared.RunProcess'))
         runProcess.setIcon(QtGui.QIcon(getFile('shared/icons/start.svg')))
         runProcess.setToolButtonStyle(Qt.ToolButtonIconOnly)
-        runProcess.setFixedSize(QtCore.QSize(15, 15))
-        runProcess.setIconSize(QtCore.QSize(15, 15))
 
         # Pack all default actions
         self.addWidget(openFolder)
